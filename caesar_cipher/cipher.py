d6972f2e26926e682773b71000b792b8346c8bfb
Katrina Hill <khill365@gmail.com>
2022-05-11 14:36:02 -0700

    added functions for encrypt, decrypt, and crack

---
+++ caesar_cipher/cipher.py
@@ -0,0 +1,14 @@
+import random
+import re
+from corpus_loader import word_list, name_list
+
+
+
+def encrypt(plain, shift):
+    pass
+
+def decrypt(encoded, shift):
+    pass
+
+def crack():
+    pass
