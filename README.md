6fb5fd30eb3f9977ae4a11f7288574a4114ac3bd
Katrina Hill <khill365@gmail.com>
2022-05-11 16:06:01 -0700

    fixed pytest, added README notes, added more to encrypt method

---
+++ README.md
@@ -0,0 +1,13 @@
+# Lab 18: Cryptography
+
+## Overview
+
+- Devise a method to encrypt a message that can then be decrypted when supplied with the corresponding key.
+- Devise a way to decipher the encrypted message event when you do NOT have the key!
+
+## Feature Tasks
+
+- Create an encrypt function that takes in a plain text phrase and a numeric shift.  
+- Create a decrypt function that takes in encrypted text and numeric shift which will restore the encrypted text back to its original form when correct key is supplied.  
+- Create a crack function that will decode the cipher so that an encrypted message can be transformed into its original state WITHOUT access to the key.  
+- Devise a method for the computer to determine if code was broken with minimal human guidance.
--- caesar_cipher/cipher.py
+++ caesar_cipher/cipher.py
@@ -3,12 +3,19 @@ import re
 from corpus_loader import word_list, name_list
 
 
+# function takes in a plain text phrase and a numeric shift. 
+def encrypt(plain, shift): 
+    encrypted_text = ""
+    for char in plain:
+        letters = int(char)
+        shifted_letters = (letters + shift) % 10
+        encrypted_text += str(shifted_letters)
+    return encrypted_text
 
-def encrypt(plain, shift):
-    pass
+# function takes in encrypted text and numeric shift which will restore the encrypted text back to its original form when correct key is supplied.
+def decrypt(encoded, shift): 
+    return encrypt(encoded, shift)
 
-def decrypt(encoded, shift):
-    pass
-
-def crack():
+# function will decode the cipher so that an encrypted message can be transformed into its original state WITHOUT access to the key.
+def crack(encoded): 
     pass
